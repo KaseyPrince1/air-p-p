60200d995b278318dfb0a18b9adf532ba47c4868
Gold Dust <48570104+ashley-blue@users.noreply.github.com>
2019-09-07 09:13:16 -0600

    minor changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require('path');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 3306;
 const db = require("./models");
 
 app.use(express.static(path.join(__dirname, "public")));
