bcdfcee2c3126bf8e60fda444660542c3b75e3b9
Kaseyprince1 <kaseyprince1995@gmail.com>
2019-09-07 02:30:16 -0600

    /models/Bathrooms.js and locations.js and  review.js and user.js /public/bathroom.html and review.html and bathroom.js and review.js +additional comments, thoughts and anything else not mentioned above.

--- public/assets/js/bathrooms.js
+++ public/assets/js/bathrooms.js
@@ -1,20 +1,18 @@
 const url_string = window.location.href;
 const url = new URL(url_string);
 const mallId = url.searchParams.get("id");
-console.log(mallId);
+console.log('');
 
 
-function fetchMallById() {
-    fetch(`/api/location/single/${mallId}`)
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => console.error(error));
-}
-
-fetchMallById();
+// function bathroom() {
+//     fetch(`/api/location/single/`)
+//         .then(res => {
+//             if (res.ok) {
+//                 return res.json();
+//             }
+//         })
+//         .then(data => {
+//             console.log(data);
+//         })
+//         .catch(error => console.error(error));
+// }
